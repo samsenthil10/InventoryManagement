8853cfb05748e6ca033e9011353421030ea56015
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 04:22:50 +0530

    Modified: The console query asked to user

--- src/com/bridgelabz/inventorymanagement/InventoryFactory.java
+++ src/com/bridgelabz/inventorymanagement/InventoryFactory.java
@@ -10,16 +10,16 @@ public class InventoryFactory {
 	LinkedList<Inventory> myInventory = new LinkedList<Inventory>();
 
 	public LinkedList<Inventory> getInventory() {
-		System.out.println("Enter total number of inventories: ");
+		System.out.print("Enter total number of inventories: ");
 		Scanner scanner = new Scanner(System.in);
 		int count = scanner.nextInt();
 
 		for (int index = 0; index < count; index++) {
-			System.out.print("Enter the name of Inventory: ");
+			System.out.print("Enter the name of inventory: ");
 			String name = scanner.next();
-			System.out.print("Enter weight in kg: ");
+			System.out.print("Enter the total weight of "+name+" in kg: ");
 			double weight = scanner.nextDouble();
-			System.out.print("Enter the price per kg: ");
+			System.out.print("Enter the price of "+name+" per kg: ");
 			double price = scanner.nextDouble();
 			Inventory newInventory = new Inventory();
 			newInventory.setName(name);
@@ -28,6 +28,7 @@ public class InventoryFactory {
 			Node<Inventory> myStockNode = new Node<Inventory>(newInventory);
 			myInventory.appendNode(myStockNode);
 		}
+		
 		scanner.close();
 		return myInventory;
 	}
