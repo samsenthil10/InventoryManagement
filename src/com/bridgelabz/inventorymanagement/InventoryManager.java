a5ead6648a22bca84190dcc43fe62146aad0d29e
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 04:38:04 +0530

    Deleted: Calculate function to abstract the method

--- src/com/bridgelabz/inventorymanagement/InventoryManager.java
+++ src/com/bridgelabz/inventorymanagement/InventoryManager.java
@@ -1,25 +1,14 @@
 package com.bridgelabz.inventorymanagement;
 
 import com.bridgelabz.datastructures.LinkedList;
-import com.bridgelabz.datastructures.Node;
 
 public class InventoryManager {
 
 	LinkedList<Inventory> myInventory;
-
-	public void addInventory() {
+	
+	public LinkedList<Inventory> addInventory() {
 		InventoryFactory inventoryFactory = new InventoryFactory();
 		this.myInventory = inventoryFactory.getInventory();
-	}
-
-	public void calculateValue() {
-		
-		Node<Inventory> tempNode = (Node<Inventory>) myInventory.head;
-		while (tempNode != null) {
-			Inventory current = tempNode.getKey();
-			double value = (current.getPrice() * current.getWeight());
-			System.out.println("Total value for inventory " + current.getName() + " is: " + value);
-			tempNode = (Node<Inventory>) tempNode.getNext();
-		}
+		return myInventory;
 	}
 }
